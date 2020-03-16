823065fb6e26329e0e5bf3f5467766215e067d05
chrismcglynn <chris@mcglynnenterprises.com>
2020-03-16 16:10:13 -0400

    remove ability to zoom in on mobile

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -16,6 +16,7 @@ export function wrapPageElement({ element, props }) {
       <Helmet>
         <meta charSet="utf-8" />
         <title>Chris McGlynn</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1,user-scalable=0"/>
         <link rel="canonical" href="http://mysite.com/example" />
       </Helmet>
       <Layout {...props}>{element}</Layout>
